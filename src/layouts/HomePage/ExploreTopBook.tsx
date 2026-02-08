cd7bd2a12bad523f5ee168be342557b9259e1d60
Saniya <saniyabagwan1111@gmail.com>
2026-02-08 23:19:12 +0530

    Created Reach app and header and Explorebook componenet using typescript

---
+++ src/layouts/HomePage/ExploreTopBook.tsx
@@ -0,0 +1,13 @@
+export const ExploreTopBook=()=>{
+    return(
+        <div className="p5 mb-4 bg-dark header">
+            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold">Find your next adventure</h1>
+                    <p className="col-md-8 fs-4"> Where would you like to go next?</p>
+                    <a className="btn main-color btn-lg text-white" href='#' type="button">Explore Top Books</a>
+                </div>
+            </div>
+        </div>
+    )
+}
