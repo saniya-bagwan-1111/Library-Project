9abc4ed201ba7be58d2bbe53795702594c956ef2
Saniya <saniyabagwan1111@gmail.com>
2026-02-11 23:14:47 +0530

    add seperate component for carousel item

--- src/layouts/HomePage/Carousel.tsx
+++ src/layouts/HomePage/Carousel.tsx
@@ -1,3 +1,5 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
     return (
         <div className='container mt-5' style={{ height: 550 }} >
@@ -11,46 +13,22 @@ export const Carousel = () => {
                 <div className='carousel-inner'>
                     <div className='carousel-item active'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-                                <div className='text-center'>
-                                    <img src={require('../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151' height='233'
-                                        alt='book' />
-                                    <h6 className='mt-2'>Saniya</h6>
-                                    <p>React app</p>
-                                    <a className='btn main-color text-white' href='#'>Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook/>
                         </div>
                     </div>
 
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-                                <div className='text-center'>
-                                    <img src={require('../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151' height='233'
-                                        alt='book' />
-                                    <h6 className='mt-2'>aniya</h6>
-                                    <p>React app2</p>
-                                    <a className='btn main-color text-white' href='#'>Reserve</a>
-                                </div>
-                            </div>
+                            <ReturnBook/>
+                            <ReturnBook/>
                         </div>
                     </div>
 
                     <div className='carousel-item'>
                         <div className='row d-flex justify-content-center align-items-center'>
-                            <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-                                <div className='text-center'>
-                                    <img src={require('../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151' height='233'
-                                        alt='book' />
-                                    <h6 className='mt-2'>niya</h6>
-                                    <p>React app 3</p>
-                                    <a className='btn main-color text-white' href='#'>Reserve</a>
-                                </div>
-                            </div>
+                           <ReturnBook/>
+                           <ReturnBook/>
+                           <ReturnBook></ReturnBook>
                         </div>
                     </div>
                 </div>
@@ -70,19 +48,10 @@ export const Carousel = () => {
             {/* Mobile */}
             <div className="d-lg-none mt-3">
                 <div className="row d-flex justify-content-center align-items-center">
-                    <div className="text-center">
-                        <img src={require('../../Images/BooksImages/book-luv2code-1000.png')}
-                            width='131'
-                            height='233'
-                            alt="book" />
-                        <h6 className="mt-2">
-                            <b>Book</b>
-                        </h6>
-                        <p>L2 c</p>
-                        <a href="" className="btn main-color text-white">Reserve</a>
-
-                    </div>
-                </div>
+                <ReturnBook/>
+                <ReturnBook/>
+                <ReturnBook/>
+            </div>
             </div>
             <div className="homepage-carousel-title mt-3">
                 <a href="#" className="btn btn-outline-secondary btn-lg">View More</a>
--- src/layouts/HomePage/ReturnBook.tsx
+++ src/layouts/HomePage/ReturnBook.tsx
@@ -2,6 +2,15 @@ import React from "react";
 
 export const ReturnBook=()=>{
     return(
-        <div></div>
+         <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
+                                <div className='text-center'>
+                                    <img src={require('../../Images/BooksImages/book-luv2code-1000.png')}
+                                        width='151' height='233'
+                                        alt='book' />
+                                    <h6 className='mt-2'>niya</h6>
+                                    <p>React app 3</p>
+                                    <a className='btn main-color text-white' href='#'>Reserve</a>
+                                </div>
+                            </div>
     );
 }
