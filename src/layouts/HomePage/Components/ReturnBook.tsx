08ad9cf3f9e318305c312dbfbbc7aff342af8f94
Saniya <saniyabagwan1111@gmail.com>
2026-03-01 00:20:46 +0530

    Fetching book img and other details displaying on frontend

--- src/layouts/HomePage/Components/ReturnBook.tsx
+++ src/layouts/HomePage/Components/ReturnBook.tsx
@@ -1,16 +1,25 @@
 import React from "react";
+import BookModel from "../../../models/BookModel";
 
-export const ReturnBook=()=>{
-    return(
-         <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
-                                <div className='text-center'>
-                                    <img src={require('../../../Images/BooksImages/book-luv2code-1000.png')}
-                                        width='151' height='233'
-                                        alt='book' />
-                                    <h6 className='mt-2'>niya</h6>
-                                    <p>React app 3</p>
-                                    <a className='btn main-color text-white' href='#'>Reserve</a>
-                                </div>
-                            </div>
+export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
+    return (
+        <div className='col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3'>
+
+            <div className='text-center'>
+                {
+                    props.book.img ?
+                        <img src={props.book.img}
+                            width='151' height='233'
+                            alt='book' />
+                        :
+                        <img src={require('../../../Images/BooksImages/book-luv2code-1000.png')}
+                            width='151' height='233'
+                            alt='book' />
+                }
+                <h6 className='mt-2'>{props.book.title}</h6>
+                <p>{props.book.author}</p>
+                <a className='btn main-color text-white' href='#'>Reserve</a>
+            </div>
+        </div>
     );
 }
