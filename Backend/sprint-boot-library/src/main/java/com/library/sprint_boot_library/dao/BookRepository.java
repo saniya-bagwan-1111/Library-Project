ab945d8ea3b39af3d6f5b1aa0ceffc228266f944
Saniya <saniyabagwan1111@gmail.com>
2026-03-02 01:11:18 +0530

    Adding Pagination on backend

--- Backend/sprint-boot-library/src/main/java/com/library/sprint_boot_library/dao/BookRepository.java
+++ Backend/sprint-boot-library/src/main/java/com/library/sprint_boot_library/dao/BookRepository.java
@@ -1,11 +1,16 @@
 package com.library.sprint_boot_library.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.library.sprint_boot_library.entity.Book;
 
 @CrossOrigin("http://localhost:3000")//react app port required 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+	Page<Book> findByTitleContaining(@RequestParam("title") 
+	String title, Pageable pageable);
 }
