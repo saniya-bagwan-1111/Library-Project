53b59c0db282a273f43af357b9054d69d63da4ac
Saniya <saniyabagwan1111@gmail.com>
2026-03-03 00:20:54 +0530

    Search book By Category

--- Backend/sprint-boot-library/src/main/java/com/library/sprint_boot_library/dao/BookRepository.java
+++ Backend/sprint-boot-library/src/main/java/com/library/sprint_boot_library/dao/BookRepository.java
@@ -13,4 +13,10 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
 	Page<Book> findByTitleContaining(@RequestParam("title") 
 	String title, Pageable pageable);
+	
+	Page<Book>  findByCategory(
+			@RequestParam("category")
+			String category,
+			Pageable pagebale
+			);
 }
